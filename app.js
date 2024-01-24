33f664e0e86f0783501626839f0b18e5edf1dae3
DrejT <vivekpatel4049@gmail.com>
2024-01-24 09:51:48 +0530

    reset

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ dotenv.config();
 
 const wss = new WebSocketServer({ port: process.env.PORT });
 
-wss.on("connection", function connection(ws: WebSocketServer) {
+wss.on("connection", function connection(ws) {
+  // enable sending json
+  ws.json = (obj) => ws.send(JSON.stringify(obj));
   ws.on("message", function message(data) {
     console.log("received: %s", data);
   });
